14c2df1873b301ed49d159a4a17bd85da1f73b0f
Riyan Maknojia <86026506+RiyanMak@users.noreply.github.com>
2024-01-15 21:03:05 -0600

    Add files via upload
    converted login html file to php

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Axis Chat App</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form login ">
+            <header>Axis-Chat App</header>
+            <form action="#">
+                <div class="error-txt">This is an Error Message!</div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" placeholder="Enter your email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password">
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Not yet signed up? <a href="#">Signup now</a></div>
+        </section>
+    </div>
+
+    <script src="javascript/pass-show-hide.js"></script>
+
+</body>
+</html>
