ed4af456a0f911b9b95a47b2f6e14e168d60e4a3
Riyan Maknojia <86026506+RiyanMak@users.noreply.github.com>
2024-01-30 17:25:04 -0600

    check the user email valid and check if it exist

--- signup.php
+++ signup.php
@@ -1,7 +1,24 @@
 <?php 
     include_once "config.php";
-    $fname = mysqli_real_escape_string($conn,$_POST['']);
-    $lname = mysqli_real_escape_string($conn,$_POST['']);
-    $email = mysqli_real_escape_string($conn,$_POST['']);
-    $password = mysqli_real_escape_string($conn,$_POST['']);
+    $fname = mysqli_real_escape_string($conn,$_POST['fname']);
+    $lname = mysqli_real_escape_string($conn,$_POST['lname']);
+    $email = mysqli_real_escape_string($conn,$_POST['email']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+
+    if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+            //$sql = mysqli_query($conn, "SELECT email FROM");
+        }else{
+            echo "$email - This is not a valid email! "
+
+        }
+
+
+
+    }else{
+        echo "All input field are required! ";
+    }
+    
 ?>
