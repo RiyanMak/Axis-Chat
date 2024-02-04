2b694c073de2c8d4075f3aa780c172c29c83fe2e
Riyan Maknojia <86026506+RiyanMak@users.noreply.github.com>
2024-02-03 19:16:29 -0600

    User uploaded image code done

--- signup.php
+++ signup.php
@@ -9,7 +9,28 @@
         
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
 
-            //$sql = mysqli_query($conn, "SELECT email FROM");
+            $sql = mysqli_query($conn, "SELECT email FROM users WHERE email = '{$email}' ");
+            if(mysqli_num_rows($sql) > 0){
+                echo "$email - already exist!";
+            }else{
+                if(isset($_FILES['image'])){
+                    $img_name = $_FILES['image']['name'];
+                    $img_type = $_FILES['image']['type'];
+                    $tmp_name = $_FILES['image']['tmp_name'];
+
+                    $img_explode = explode('.',$img_name);
+                    $img_ext = end($img_explode);
+
+                    $extensions = ['png' , 'jpeg', 'jpg'];
+                    if(in_array($img_ext,$extensions) === true){
+
+                    }else{
+                        
+                    }
+                }else{
+                    echo "Please select an Image File!";
+                }
+            }
         }else{
             echo "$email - This is not a valid email! "
 
